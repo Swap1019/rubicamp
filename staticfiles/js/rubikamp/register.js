c190237901c0feec73ececab5a92ca3154ba253a
Swap1019 <your_github_email>
2025-05-09 03:18:22 +0330

    ready to deploy

---
+++ staticfiles/js/rubikamp/register.js
@@ -0,0 +1,18 @@
+document.getElementById("id_profile").addEventListener("change", function (event) {
+  const preview = document.getElementById("profile-preview");
+  const file = event.target.files[0];
+  
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+      preview.style.display = "block";
+    };
+    reader.readAsDataURL(file);
+  } else {
+    preview.style.display = "none";
+  }
+});
+
+
+
