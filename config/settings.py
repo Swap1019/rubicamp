323d19251f87c5c239bdc018acda05ed2d92af82
Swap1019 <your_github_email>
2025-05-09 04:01:54 +0330

    ready to deploy 5

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'rubikamp.User'
 AUTH_PROFILE_MODULE = 'profiles.User'
